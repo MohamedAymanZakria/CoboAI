7cc0fb0ce6d131421b292d39e212abcbc24259c5
Mohamed Ayman Zakria <106837274+MohamedAymanZakria@users.noreply.github.com>
2025-07-18 20:17:39 +0300

    Add AI Assets view using Syncfusion

--- CoboAI/MauiProgram.cs
+++ CoboAI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace CoboAI
 {
@@ -9,6 +10,7 @@ namespace CoboAI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
